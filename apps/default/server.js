b9756317eed27638e42a2b8cf94375ec614ee9d0
woloski <matias.woloski@southworks.net>
2012-05-15 09:21:14 -0300

    # from win

--- apps/default/server.js
+++ apps/default/server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function(req, res) {
 	res.writeHead(200);
-	res.end('hello world azure.\ncurrent time on server: ' + new Date());
+	res.end('hello world azure from win.\ncurrent time on server: ' + new Date());
 }).listen(process.env.PORT || 3708);
