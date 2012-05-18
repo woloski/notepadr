a624e4c17ea5fad7f056638c9aae2a5da28e5c2d
woloski <matias.woloski@southworks.net>
2012-05-18 17:17:35 -0300

    notepadr

---
+++ apps/notepadrdemo/server.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var express = require('express');
+var fs = require('fs');
+var share = require('share');
+var mustache = require('mustache');
+
+var doc = fs.readFileSync('doc.html', 'utf8');
+
+var app = express.createServer();
+
+app.get('/', function(req, res) {
+    res.redirect('/new');
+});
+
+app.get('/:docName', function(req, res) {
+    var html = mustache.to_html(doc, { docname: req.params.docName });
+    console.log(' new req' + req.url);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(html);
+
+});
+var port = process.env.PORT || 8080;
+share.server.attach(app, { db: { type: 'none' }, port: port })
+app.listen(port);
